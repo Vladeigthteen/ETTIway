6df0bd38651717bc10f287f9515730e6f2bf4c48
Vladeigthteen <fraticiuvlad6@gmail.com>
2026-05-26 19:05:38 +0300

    update error

--- Backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ Backend/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import static org.springframework.security.config.Customizer.withDefaults;
+
 
 @Configuration
 public class SecurityConfig {
